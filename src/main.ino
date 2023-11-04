d97583260ecf101fb728790b313ee6dec7a14c66
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-11-04 14:36:53 -0300

    fix: improvements
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- src/main.ino
+++ src/main.ino
@@ -1,31 +1,31 @@
 #define closes 15    // D8
 #define opens 13     // D7
 #define activates 12 // D6
-#define sensor 04    // D2
+#define sensor 14    // D5
 
 void setup() {
   pinMode(closes, OUTPUT);
   pinMode(opens, OUTPUT);
   pinMode(activates, INPUT_PULLUP);
-  pinMode(sensor, INPUT);
+  pinMode(sensor, INPUT_PULLUP);
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
 }
 
 void loop() {
-  // shelly sends the pulse to D6
+  Serial.println(digitalRead(sensor));
   while (!digitalRead(activates)) {
     Serial.println("activates");
     digitalWrite(LED_BUILTIN, HIGH);
-    if (!digitalRead(sensor)) {
+    if (digitalRead(sensor)) {
       Serial.println("closes");
       digitalWrite(closes, HIGH);
     } else {
       Serial.println("opens");
       digitalWrite(opens, HIGH);
     }
+    delay(100);
   }
-  delay(500);
   digitalWrite(closes, LOW);
   digitalWrite(opens, LOW);
   digitalWrite(LED_BUILTIN, LOW);
