8f42dfe7582a63a17554f8dd251863a92139af4f
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-11-11 04:42:26 +0000

    fix

--- src/main.ino
+++ src/main.ino
@@ -88,7 +88,7 @@ void loop() {
 void pubSensor() {
   String status = digitalRead(SENSOR) ? "open" : "closed";
   if (status != lastStatus) {
-    client.publish("espgate/sensor", digitalRead(SENSOR) ? "open" : "closed");
+    client.publish("espgate/sensor", status);
     lastStatus = status;
   }
 }
