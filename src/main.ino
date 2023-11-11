9dece533100c7abbb448114a41fa36e9b7beac5b
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-11-11 01:33:47 -0300

    wip: homekit 3rd party svc via mqtt
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- src/main.ino
+++ src/main.ino
@@ -1,23 +1,25 @@
-#define closes 15    // D8
-#define opens 13     // D7
-#define sensor 14    // D5
+#define CLOSES 15    // D8
+#define OPENS 13     // D7
+#define SENSOR 14    // D5
+#define PULSE 500
 
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-#include <ESP8266WebServer.h>
+#include <PubSubClient.h>
 #include "arduino_secrets.h"
 
 const char* ssid = WIFI_SSID;
 const char* password = WIFI_PASS;
+const char* mqttHost = MQTT_HOST;
 
-ESP8266WebServer server(80);
 WiFiClient net;
-String lastAction = "none";
+PubSubClient client(net);
+String lastStatus = "unknown";
 
 void setup() {
-  pinMode(closes, OUTPUT);
-  pinMode(opens, OUTPUT);
-  pinMode(sensor, INPUT_PULLUP);
+  pinMode(CLOSES, OUTPUT);
+  pinMode(OPENS, OUTPUT);
+  pinMode(SENSOR, INPUT_PULLUP);
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
 
@@ -35,65 +37,76 @@ void setup() {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  server.on("/", handleRoot);
-  server.on("/open", handleOpen);
-  server.on("/close", handleClose);
-  server.on("/sensor", handleSensor);
-  server.begin();
-  Serial.println("HTTP server started");
+  client.setServer(mqttHost, 1883);
+  client.setCallback(callback);
   digitalWrite(LED_BUILTIN, HIGH);
 }
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("got message: ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  if ((char)payload[0] == 'p') { // ping
+    lastStatus = "unknown";
+    pubSensor();
+  } else if ((char)payload[0] == 'o') { // open
+    handleOpen();
+  } else { // close
+    handleClose();
+  }
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    if (client.connect("espgate")) {
+      Serial.println("connected");
+      client.subscribe("espgate/act");
+      lastStatus = "unknown";
+      pubSensor();
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
 
 void loop() {
-  server.handleClient();
+  if (!client.connected()) {
+    reconnect();
+  }
+  pubSensor();
+  client.loop();
+  delay(100);
 }
 
-void handleRoot() {
-  digitalWrite(LED_BUILTIN, LOW);
-  String body = "<html>";
-  body += "<head><title>Gate Status</title><head>";
-  body += "<body>";
-  body += "<h1>Gate ESP8266 Status</h1>\n";
-  body += "<p>Sensor status: <b>";
-  body += digitalRead(sensor) ? "open" : "closed";
-  body += "</b></p>\n";
-  body += "<p>Last action: <b>"+lastAction+"</b></p>\n";
-  body += "<p>";
-  body += "<a href=\"/open\">Open</a>";
-  body += " | ";
-  body += "<a href=\"/close\">Close</a>";
-  body += "</p>\n";
-  body += "</body>";
-  body += "</html>";
-  server.send(200, "text/html", body);
-  digitalWrite(LED_BUILTIN, HIGH);
+void pubSensor() {
+  String status = digitalRead(SENSOR) ? "open" : "closed";
+  if (status != lastStatus) {
+    client.publish("espgate/sensor", digitalRead(SENSOR) ? "open" : "closed");
+    lastStatus = status;
+  }
 }
-
-void handleSensor() {
-  server.send(200, "text/html", digitalRead(sensor) ? "open" : "closed");
-}
-
 
 void handleOpen() {
   Serial.println("opening");
   digitalWrite(LED_BUILTIN, LOW);
-  digitalWrite(opens, HIGH);
-  delay(500);
-  digitalWrite(opens, LOW);
-  lastAction = "open";
+  digitalWrite(OPENS, HIGH);
+  delay(PULSE);
+  digitalWrite(OPENS, LOW);
   digitalWrite(LED_BUILTIN, HIGH);
-  server.sendHeader("Location","/");
-  server.send(303);
 }
 
 void handleClose() {
   Serial.println("closing");
   digitalWrite(LED_BUILTIN, LOW);
-  digitalWrite(closes, HIGH);
-  delay(500);
-  digitalWrite(closes, LOW);
-  lastAction = "close";
+  digitalWrite(CLOSES, HIGH);
+  delay(PULSE);
+  digitalWrite(CLOSES, LOW);
   digitalWrite(LED_BUILTIN, HIGH);
-  server.sendHeader("Location","/");
-  server.send(303);
 }
